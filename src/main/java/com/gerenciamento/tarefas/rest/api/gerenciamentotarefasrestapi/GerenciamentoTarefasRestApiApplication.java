959310a557dba071b7da5784bb07604745849d97
IngredyMartins <gabi_ingredy@hotmail.com>
2023-06-24 21:25:36 -0300

    Criação do projeto Spring Boot

---
+++ src/main/java/com/gerenciamento/tarefas/rest/api/gerenciamentotarefasrestapi/GerenciamentoTarefasRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.tarefas.rest.api.gerenciamentotarefasrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoTarefasRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoTarefasRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciamento/tarefas/rest/api/gerenciamentotarefasrestapi/GerenciamentoTarefasRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.tarefas.rest.api.gerenciamentotarefasrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoTarefasRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
